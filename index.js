2fe323482c4d3049a84c296ad47f78e2ec8705f0
Gabsop <gabsop@hotmail.com>
2022-07-14 02:48:12 -0300

    Changed to receive an array of properties and make xlsx with them

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
 import scrape from "./src/scrape.js";
+import XLSX from "xlsx";
 
-const checkin = new Date(2022, 8, 12);
+const checkin = new Date(2022, 8, 14);
 const checkout = new Date(2022, 8, 16);
 const quartos = "1";
 const adultos = "1";
+const hotels = ["Lagarto Na Banana Hostel", "Hostel Casa de Jack", "Casa Kokoro"];
+let results = [];
 
-await scrape(checkin, checkout, quartos, adultos);
+try {
+    for (const hotel of hotels) {
+        let result = await scrape(checkin, checkout, quartos, adultos, hotel);
+        results.push(result);
+    };
+} catch (error) {
+    throw error;
+}
+
+console.log(Object.assign({}, results));
+
+const resultsWorkbook = XLSX.utils.book_new();
+const resultsSheet = XLSX.utils.json_to_sheet(results);
+
+XLSX.utils.book_append_sheet(resultsWorkbook, resultsSheet);
+XLSX.writeFile(
+    resultsWorkbook,
+    `${("0" + checkin.getDate()).slice(-2) +
+    "-" +
+    ("0" + checkin.getMonth()).slice(-2) +
+    "-" +
+    checkin.getFullYear()
+    }__${("0" + checkout.getDate()).slice(-2) +
+    "-" +
+    ("0" + checkout.getMonth()).slice(-2) +
+    "-" +
+    checkout.getFullYear()
+    }.xlsx`
+);
--- src/scrape.js
+++ src/scrape.js
@@ -1,9 +1,8 @@
 import puppeteer from "puppeteer-extra";
-import XLSX from "xlsx";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
 
-export default async function scrape(checkin, checkout, quartos, adultos) {
-  if (!checkin || !checkout || !quartos || !adultos) {
+export default async function scrape(checkin, checkout, quartos, adultos, hotel) {
+  if (!checkin || !checkout || !quartos || !adultos || !hotel) {
     throw new Error("Missing required parameters");
   }
 
@@ -11,115 +10,70 @@ export default async function scrape(checkin, checkout, quartos, adultos) {
     puppeteer.use(StealthPlugin());
     const browser = await puppeteer.launch({
       headless: true,
-      args: ["--start-maximized", "--no-sandbox"],
+      // args: ['--start-maximized']
     });
     const page = await browser.newPage();
-    await page.setViewport({ width: 1366, height: 768 });
+    await page.setViewport({ width: 2560, height: 1600 });
     await page.setUserAgent(
       "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
     );
 
-    const url = `https://www.booking.com/searchresults.pt-br.html?label=gen173nr-1DCAEoggI46AdIM1gEaCCIAQGYAS24ARfIARXYAQPoAQGIAgGoAgO4ApivspYGwAIB0gIkZmQxNzkwOGYtZDQ1ZS00N2JlLTkwNDYtZWEwNGQzNjhiZjQx2AIE4AIB&sid=3ae7520e20e7f5ecc6f0df18e53e7fa6&sb=1&sb_lp=1&src=index&src_elem=sb&error_url=https%3A%2F%2Fwww.booking.com%2Findex.pt-br.html%3Flabel%3Dgen173nr-1DCAEoggI46AdIM1gEaCCIAQGYAS24ARfIARXYAQPoAQGIAgGoAgO4ApivspYGwAIB0gIkZmQxNzkwOGYtZDQ1ZS00N2JlLTkwNDYtZWEwNGQzNjhiZjQx2AIE4AIB%26sid%3D3ae7520e20e7f5ecc6f0df18e53e7fa6%26sb_price_type%3Dtotal%26%26&ss=Pipa&is_ski_area=0&ssne=Pipa&ssne_untouched=Pipa&dest_id=-662045&dest_type=city&checkin_year=${checkin.getFullYear()}&checkin_month=${checkin.getMonth()}&checkin_monthday=${checkin.getDate()}&checkout_year=${checkout.getFullYear()}&checkout_month=${checkout.getMonth()}&checkout_monthday=${checkout.getDate()}&group_adults=${adultos}&group_children=0&no_rooms=${quartos}&b_h4u_keep_filters=&from_sf=1`;
-
-    // const pagesXPATH =
-    //   "//*[@id='search_results_table']/div[2]/div/div/div/div[7]/div[2]/nav/div/div[2]";
-
+    const url = `https://www.booking.com/searchresults.pt-br.html?label=gen173nr-1DCAEoggI46AdIM1gEaCCIAQGYAS24ARfIARXYAQPoAQGIAgGoAgO4ApivspYGwAIB0gIkZmQxNzkwOGYtZDQ1ZS00N2JlLTkwNDYtZWEwNGQzNjhiZjQx2AIE4AIB&sid=3ae7520e20e7f5ecc6f0df18e53e7fa6&sb=1&sb_lp=1&src=index&src_elem=sb&error_url=https%3A%2F%2Fwww.booking.com%2Findex.pt-br.html%3Flabel%3Dgen173nr-1DCAEoggI46AdIM1gEaCCIAQGYAS24ARfIARXYAQPoAQGIAgGoAgO4ApivspYGwAIB0gIkZmQxNzkwOGYtZDQ1ZS00N2JlLTkwNDYtZWEwNGQzNjhiZjQx2AIE4AIB%26sid%3D3ae7520e20e7f5ecc6f0df18e53e7fa6%26sb_price_type%3Dtotal%26%26&ss=Pipa&is_ski_area=0&ssne=Pipa&ss=Pipa&dest_id=-662045&dest_type=city&checkin_year=${checkin.getFullYear()}&checkin_month=${checkin.getMonth()}&checkin_monthday=${checkin.getDate()}&checkout_year=${checkout.getFullYear()}&checkout_month=${checkout.getMonth()}&checkout_monthday=${checkout.getDate()}&group_adults=${adultos}&group_children=0&no_rooms=${quartos}&b_h4u_keep_filters=&from_sf=1`;
     const tableXPATH = "//*[@id='search_results_table']";
+    const inputSelector = ".ce45093752";
+    const buttonSelector = "#left_col_wrapper > div:nth-child(1) > div > div > form > div > div:nth-child(6) > div > button";
+    const optionSelector = "#left_col_wrapper > div:nth-child(1) > div > div > form > div > div:nth-child(2) > div > div.ab090fee6e.cc6f7f2b89 > div.a7631de79e > ul > li:nth-child(1) > div";
 
-    console.log("Starting scraping...");
+    console.log("Starting scraping for", hotel);
 
     await page.goto(url);
-    await page.waitForXPath(tableXPATH, { timeout: 1000 });
+    await page.waitForSelector(inputSelector, { timeout: 90000 });
+    await page.waitForSelector(buttonSelector, { timeout: 90000 });
 
-    console.log("Table found!");
+    await page.click(inputSelector, { clickCount: 3 });
+    await page.type(inputSelector, hotel);
+    await page.waitForSelector(optionSelector, { timeout: 90000 });
+    await page.click(optionSelector);
 
-    // await page.waitForXPath(pagesXPATH, { timeout: 10000 });
-    // let pagesHandler = await page.$x(pagesXPATH);
-    // let pages = await page.evaluate((el) => el.textContent, pagesHandler[0]);
-    // console.log("Pages: ", pages);
+    await page.click(buttonSelector);
 
-    // const regex = /[0-9](?=…)/gm;
-    // let pagesAmount = pages.match(regex);
-    // console.log("Pages Amount: ", pagesAmount[0]);
+    await page.waitForNavigation({ waitUntil: 'networkidle2' });
+    await page.waitForXPath(tableXPATH, { timeout: 90000 });
 
-    const names = await page.$$eval(".fcab3ed991.a23c043802", (el) =>
-      el.map((ell) => ell.innerHTML)
-    );
 
-    const values = await page.$$eval(".fcab3ed991.bd73d13072", (el) =>
-      el.map((ell) => ell.innerHTML)
-    );
+    const name = await page.$eval(".fcab3ed991.a23c043802", (el) => el.innerHTML);
 
-    const description = await page.$$eval(".df597226dd", (el) =>
-      el.map((ell) => ell.innerHTML)
-    );
+    const price = await page.$eval(".fcab3ed991.bd73d13072", (el) => el.innerHTML);
 
-    // const layout = await page.$$eval(".cb5ebe3ffb", (el) =>
-    //   el.map((ell) => ell.innerHTML)
-    // );
+    const description = await page.$eval(".df597226dd", (el) => el.innerHTML);
 
-    const rates = await page.$$eval(".b5cd09854e.d10a6220b4", (el) =>
-      el.map((ell) => ell.innerHTML)
-    );
+    const rate = await page.$eval(".b5cd09854e.d10a6220b4", (el) => el.innerHTML);
 
-    const breakfast = await page.$$eval(".e05969d63d", (el) =>
-      el.map((ell) => ell.innerHTML)
-    );
+    const breakfast = await page.$eval(".e05969d63d", (el) => el.innerHTML);
 
-    const beachDistance = await page.$$eval(".acb0d5ead1", (el) =>
-      el.map((ell) => ell.innerHTML)
-    );
+    const beach = await page.$eval(".acb0d5ead1", (el) => el.innerHTML);
 
-    const centerDistance = await page.$$eval("[data-testid='distance']", (el) =>
-      el.map((ell) => ell.innerHTML)
-    );
+    const center = await page.$eval("[data-testid='distance']", (el) => el.innerHTML);
 
-    const link = await page.$$eval(".e13098a59f", (el) =>
-      el.map((ell) => ell.href)
-    );
-
-    let results = [];
+    const link = await page.$eval(".e13098a59f", (el) => el.href);
 
     const reg = /&nbsp;/gm;
-    names.forEach((_, idx) =>
-      results.push({
-        Nome: names[idx].replace(reg, ""),
-        Descrição: description[idx].replace(reg, ""),
-        Valor: parseFloat(
-          values[idx].replace(reg, "").replace("R$", "").replace(".", "")
-        ),
-        // layout: layout[idx].replace(reg, ""),
-        Nota: parseFloat(rates[idx].replace(reg, "").replace(",", ".")),
-        "Café da Manhã?": breakfast[idx] ? "Sim" : "Não",
-        "Distância até a Praia": beachDistance[idx].replace(reg, ""),
-        "Distância até o Centro": centerDistance[idx].replace(reg, ""),
-        Link: link[idx].replace(reg, ""),
-      })
-    );
+    let result = {
+      Nome: name.replace(reg, ""),
+      Descrição: description.replace(reg, ""),
+      Valor: parseFloat(
+        price.replace(reg, "").replace("R$", "").replace(".", "")
+      ),
+      Nota: parseFloat(rate.replace(reg, "").replace(",", ".")),
+      "Café da Manhã?": breakfast ? "Sim" : "Não",
+      "Distância até a Praia": beach.replace(reg, ""),
+      "Distância até o Centro": center.replace(reg, ""),
+      Link: link.replace(reg, ""),
+    };
 
     await browser.close();
-    console.log("Results: ", results);
+    return result;
 
-    const resultsWorkbook = XLSX.utils.book_new();
-    const resultsSheet = XLSX.utils.json_to_sheet(results);
-
-    XLSX.utils.book_append_sheet(resultsWorkbook, resultsSheet);
-    XLSX.writeFile(
-      resultsWorkbook,
-      `${
-        ("0" + checkin.getDate()).slice(-2) +
-        "-" +
-        ("0" + checkin.getMonth()).slice(-2) +
-        "-" +
-        checkin.getFullYear()
-      }__${
-        ("0" + checkout.getDate()).slice(-2) +
-        "-" +
-        ("0" + checkout.getMonth()).slice(-2) +
-        "-" +
-        checkout.getFullYear()
-      }.xlsx`
-    );
   } catch (error) {
     console.error(error);
   }
